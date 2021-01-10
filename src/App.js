42bf247dd6d3b3c214caca6e95dab714eee3e4a6
Jess2 <jess2.developer@gmail.com>
2021-01-10 19:55:51 +0900

    chore: 불필요한 파일 제거 및 필요한 모듈 설치 및 설정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+export default function App() {
+  return <div>test</div>;
 }
-
-export default App;
