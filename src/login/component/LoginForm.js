74d67cbe5c9bdd5d0b1344b4d1bb469f5f67ee3c
Jess2 <jess2.developer@gmail.com>
2021-01-11 20:51:16 +0900

    feat: LoginForm 컴포넌트 추가

---
+++ src/login/component/LoginForm.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+import Button from '../../common/component/Button';
+
+const StyledForm = styled.form`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 60px;
+  padding: 0 2vw;
+  background-color: #ffffff;
+  border-bottom: 1px solid #e9ecef;
+`;
+
+export default function LoginForm() {
+    return <>
+        <StyledForm>
+            <label htmlFor={'user-id'}>
+                <input id={'user-id'} type={'text'} placeholder={'Email ID'}/>
+            </label>
+            <label htmlFor={'user-pw'}>
+                <input id={'user-pw'} type={'text'} placeholder={'Password'}/>
+            </label>
+            <Button type={'submit'}>SIGN IN</Button>
+        </StyledForm>
+    </>;
+}
