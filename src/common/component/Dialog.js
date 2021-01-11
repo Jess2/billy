85683a47a4c4db9789b4d67afa031d4fba5e56a0
Jess2 <jess2.developer@gmail.com>
2021-01-11 22:34:03 +0900

    style: 모달 트랜지션 추가

--- src/common/component/Dialog.js
+++ src/common/component/Dialog.js
@@ -1,6 +1,42 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState, useEffect } from 'react';
+import styled, { css, keyframes } from 'styled-components';
 import Button from "./Button";
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  } 
+`;
+
+const fadeOut = keyframes`
+  from {
+    opacity: 1
+  }
+  to {
+    opacity: 0
+  }
+`;
+
+const slideUp = keyframes`
+  from {
+    transform: translateY(200px);
+  }
+  to {
+    transform: translateY(0px);
+  }
+`;
+
+const slideDown = keyframes`
+  from {
+    transform: translateY(0px);
+  }
+  to {
+    transform: translateY(200px);
+  }
+`;
 
 const DarkBackground = styled.div`
   position: fixed;
@@ -12,6 +48,17 @@ const DarkBackground = styled.div`
   align-items: center;
   justify-content: center;
   background: rgba(0, 0, 0, 0.8);
+  
+  animation-duration: 0.25s;
+  animation-timing-function: ease-out;
+  animation-name: ${fadeIn};
+  animation-fill-mode: forwards;
+    
+  ${props =>
+    props.disappear &&
+    css`
+      animation-name: ${fadeOut};
+    `}
 `;
 
 const DialogBlock = styled.div`
@@ -27,6 +74,17 @@ const DialogBlock = styled.div`
     font-size: 1.125rem;
     margin-top: 1rem;
   }
+  
+  animation-duration: 0.25s;
+  animation-timing-function: ease-out;
+  animation-name: ${slideUp};
+  animation-fill-mode: forwards;
+  
+  ${props =>
+    props.disappear &&
+    css`
+      animation-name: ${slideDown};
+    `}
 `;
 
 const ButtonGroup = styled.div`
@@ -43,10 +101,24 @@ const ShortMarginButton = styled(Button)`
 `;
 
 function Dialog({ title, contents, confirmText, cancelText, onConfirm, onCancel, isVisible }) {
-  if (!isVisible) return null;
+  const [animate, setAnimate] = useState(false);
+  const [localVisible, setLocalVisible] = useState(isVisible);
+
+  useEffect(() => {
+    // isVisible 값이 true -> false 가 되는 것을 감지
+    if (localVisible && !isVisible) {
+      setAnimate(true);
+      setTimeout(() => {
+        setAnimate(false)
+      }, 250);
+    }
+    setLocalVisible(isVisible);
+  }, [localVisible, isVisible]);
+
+  if (!animate && !localVisible) return null;
   return (
-    <DarkBackground>
-      <DialogBlock>
+    <DarkBackground disappear={!isVisible}>
+      <DialogBlock disappear={!isVisible}>
         <h3>{title}</h3>
         <p>{contents}</p>
         <ButtonGroup>
