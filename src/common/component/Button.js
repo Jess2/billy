6d4657116338ea74ddbc21059128704ac5b9710f
Jess2 <jess2.developer@gmail.com>
2021-01-11 20:16:00 +0900

    style: polish module 적용

--- src/common/component/Button.js
+++ src/common/component/Button.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
+import { darken, lighten } from 'polished';
 
 const StyledButton = styled.button`
   /* 공통 스타일 */
   display: inline-flex;
+  align-items: center;
   outline: none;
   border: none;
   border-radius: 4px;
@@ -15,16 +17,15 @@ const StyledButton = styled.button`
   
   /* 크기 */
   height: 2.25rem;
-  line-height: 2.1rem;
   font-size: 1rem;
 
   /* 색상 */
-  background: #228be6;
+  background: ${'#228be6'};
   &:hover {
-    background: #339af0;
+    background: ${lighten(0.1, '#228be6')};
   }
   &:focus {
-    background: #339af0;
+    background: ${darken(0.1, '#228be6')};
   }
 `;
 
