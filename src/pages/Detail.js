177643003f683f3fae4a297afbb5eeb565d4ab7a
Jess2 <jess2.developer@gmail.com>
2021-02-06 21:50:19 +0900

    fix: 최초 렌더링시 borrow 버튼이 생겼다가 사라지며 깜빡이는 현상 수정

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -14,7 +14,7 @@ const StyledWrapper = styled.div`
 
 export default function Detail({ match }) {
   const history = useHistory();
-  const [eqp, setEqp] = useState([]);
+  const [eqp, setEqp] = useState(null);
   const [borrowModal, setBorrowModal] = useState(false);
   const [deleteModal, setDeleteModal] = useState(false);
   const [selectedEqpId, setSelectedEqpId] = useState(null);
@@ -73,39 +73,43 @@ export default function Detail({ match }) {
   }, []);
 
   return (
-    <StyledWrapper>
-      <PageHeader title='장비 상세'>
-        {
-          !eqp.isBilly &&
-          <Button size='small' color='blue' outline onClick={onClickBorrow}>
-            Borrow
-          </Button>
-        }
-        {
-          eqp.isBilly && eqp.billyUser.id === myInfo.id &&
-          <Button size='small' color='blue' outline onClick={onClickBorrow}>
-            Return
-          </Button>
-        }
-        <Button size='small' color='blue' outline onClick={onClickEdit}>Edit</Button>
-        <Button size='small' color='red' outline onClick={onClickDelete}>Delete</Button>
-      </PageHeader>
-      <EqpListRow eqp={eqp} />
+    <>
+      { eqp &&
+        <StyledWrapper>
+          <PageHeader title='장비 상세'>
+            {
+              !eqp.isBilly &&
+              <Button size='small' color='blue' outline onClick={onClickBorrow}>
+                Borrow
+              </Button>
+            }
+            {
+              eqp.isBilly && eqp.billyUser.id === myInfo.id &&
+              <Button size='small' color='blue' outline onClick={onClickBorrow}>
+                Return
+              </Button>
+            }
+            <Button size='small' color='blue' outline onClick={onClickEdit}>Edit</Button>
+            <Button size='small' color='red' outline onClick={onClickDelete}>Delete</Button>
+          </PageHeader>
+          <EqpListRow eqp={eqp} />
 
-      <Modal
-        title="장비 삭제"
-        contents="정말로 삭제하시겠습니까?"
-        onConfirm={onConfirmDelete}
-        onCancel={onCancelDelete}
-        isVisible={deleteModal}
-      />
-      <Modal
-        title={ eqp.isBilly ? '장비 반납' : '장비 대여'}
-        contents={ eqp.isBilly ? '해당 장비를 반납하시겠습니까?' : '해당 장비를 대여하시겠습니까?'}
-        onConfirm={onConfirmBorrow}
-        onCancel={onCancelBorrow}
-        isVisible={borrowModal}
-      />
-    </StyledWrapper>
+          <Modal
+            title="장비 삭제"
+            contents="정말로 삭제하시겠습니까?"
+            onConfirm={onConfirmDelete}
+            onCancel={onCancelDelete}
+            isVisible={deleteModal}
+          />
+          <Modal
+            title={ eqp.isBilly ? '장비 반납' : '장비 대여'}
+            contents={ eqp.isBilly ? '해당 장비를 반납하시겠습니까?' : '해당 장비를 대여하시겠습니까?'}
+            onConfirm={onConfirmBorrow}
+            onCancel={onCancelBorrow}
+            isVisible={borrowModal}
+          />
+        </StyledWrapper>
+      }
+    </>
   );
 }
