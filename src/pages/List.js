016b2dcb0b61e1f1477d7d19b8173b4708fb7f8b
Jess2 <jess2.developer@gmail.com>
2021-02-15 12:45:28 +0900

    feat: 제조사로 리스트 검색 기능 추가

--- src/pages/List.js
+++ src/pages/List.js
@@ -5,6 +5,7 @@ import Button from "../components/atoms/Button";
 import PageHeader from "../components/molecules/PageHeader";
 import EqpTableRow from "../components/molecules/EqpTableRow";
 import {getEquipments} from "../api/equipments";
+import Input from '../components/atoms/Input';
 
 const StyledWrapper = styled.div`
   padding: 50px 0;
@@ -12,31 +13,56 @@ const StyledWrapper = styled.div`
 
 export default function List() {
   const [equipments, setEquipments] = useState([]);
+  const [searchWord, setSearchWord] = useState('');
+  const [searchedEqps, setSearchedEqps] = useState([]);
   const history = useHistory();
 
   useEffect(() => {
     getEquipments().then(data => {
       setEquipments(data);
+      setSearchedEqps(data);
     });
   }, []);
+
+  useEffect(() => {
+    setSearchedEqps(findSearchedEqp(searchWord));
+  }, [equipments]);
 
   const openEqpDetail = (eqp) => {
     history.push(`/detail/${eqp.id}`);
   };
 
+  const onChangeSearch = (e) => {
+    setSearchWord(e.target.value);
+    setSearchedEqps(findSearchedEqp(e.target.value));
+  };
+
+  const findSearchedEqp = (_searchWord) => {
+    if (_searchWord) {
+      return equipments.filter(eqp => {
+        return eqp.manufacturer.toLowerCase().includes(_searchWord.toLowerCase());
+      });
+    } else {
+      return equipments;
+    }
+  };
+
   return (
     <StyledWrapper>
       <PageHeader title='장비 목록'>
+        <Input size='small' placeholder='Search' onChange={onChangeSearch}/>
         <Link to="/create">
           <Button size='small' color='blue' outline>+ Add</Button>
         </Link>
       </PageHeader>
       <EqpTableRow isLabel={true} equipments={equipments} setEquipments={setEquipments} />
-      { equipments &&
-        equipments.map(eqp => (
+      { searchedEqps.length > 0 &&
+        searchedEqps.map(eqp => (
           <EqpTableRow key={eqp.id} eqp={eqp} openEqpDetail={openEqpDetail} />
         ))
       }
+      { equipments.length === 0 && '장비 목록 없음' }
+      { equipments.length !== 0 && searchedEqps.length === 0 && '검색 결과 없음' }
     </StyledWrapper>
   );
 }
